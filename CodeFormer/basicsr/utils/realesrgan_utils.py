d285b436aa6dcba8245abfc779fc0fb5bcb53179
sczhou <shangchenzhou@gmail.com>
2023-06-22 01:13:19 -0700

    update realesrgan_utils

--- CodeFormer/basicsr/utils/realesrgan_utils.py
+++ CodeFormer/basicsr/utils/realesrgan_utils.py
@@ -74,6 +74,7 @@ class RealESRGANer():
             self.img = self.img.half()
 
         # pre_pad
+        self.img_pre_pad = self.img.clone()
         if self.pre_pad != 0:
             self.img = F.pad(self.img, (0, self.pre_pad, 0, self.pre_pad), 'reflect')
         # mod pad for divisible borders
@@ -211,7 +212,7 @@ class RealESRGANer():
             del output_img_t
             torch.cuda.empty_cache()        
         except RuntimeError as error:
-            output_img = cv2.resize(output_img, (w_input * self.scale, h_input * self.scale), interpolation=cv2.INTER_LINEAR)
+            output_img = cv2.resize(self.img_pre_pad, (w_input * self.scale, h_input * self.scale), interpolation=cv2.INTER_LINEAR)
             print(f"Failed inference for RealESRGAN: {error}")
 
         # ------------------- process the alpha channel if necessary ------------------- #
